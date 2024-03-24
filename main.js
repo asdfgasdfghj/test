91df42cb4915382f5c5f70f86c329414dfcaa2d5
JesseIguess <blueberry21007@gmail.com>
2024-03-24 16:57:29 +0000

    funny

---
+++ main.js
@@ -0,0 +1,8 @@
+const test = [":)", ":("]
+
+function riughwoirhg() {
+    // document.write("<br>" + test[Math.floor(Math.random() * 2)])
+    document.write(Math.floor(Math.random() * 2))
+}
+
+riughwoirhg()
